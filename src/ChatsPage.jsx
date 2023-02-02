590be0bb14124d7193ea31023c79af84abda19f8
Stephen <stvlley.amzn@gmail.com>
2023-02-02 13:55:18 -0500

    init

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,22 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "e650ae84-7b12-49c3-95ff-f9ac306c39c1",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
